c80fedfd4e7e74d882ab132242ea44902df5a191
hossein <hossein.h.a770227@gmail.com>
2024-07-16 18:36:21 +0330

    add link _base.html

--- products/models.py
+++ products/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("product_detail", args=[self.pk])
+        return reverse("products:product_detail", args=[self.pk])
     
 
 class Comment(models.Model):
