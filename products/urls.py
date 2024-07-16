836b894d09b19c0c9aa41ca663fb0337b180afc6
hossein <hossein.h.a770227@gmail.com>
2024-07-16 18:11:04 +0330

    add product detail.html

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
