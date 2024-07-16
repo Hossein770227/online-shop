65eaa5a90fc9ea7137e29d42d88d3c5baec80a44
hossein <hossein.h.a770227@gmail.com>
2024-07-16 18:21:10 +0330

    add comment for product

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
@@ -21,3 +22,22 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse("product_detail", args=[self.pk])
     
+
+class Comment(models.Model):
+    SCORE_CHOICES = (
+        ('1',_('very bad')),
+        ('2',_(' bad')),
+        ('3',_('normal')),
+        ('4',_('good')),
+        ('5',_('perfect')),
+    )
+
+    author = models.ForeignKey(get_user_model(), verbose_name=_("author"), on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name=_("product"), on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField(_("text comment"))
+    score = models.CharField(choices=SCORE_CHOICES, max_length=2)
+    active = models.BooleanField(_("active"), default=True)
+    date_time_created = models.DateTimeField(_("date time created"), auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author} : {self.text}'
