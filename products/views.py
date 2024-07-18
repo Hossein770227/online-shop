a372b2cce7ddc40fae19af98e3fbb2f78653bd43
hossein <hossein.h.a770227@gmail.com>
2024-07-18 21:47:50 +0330

    add some translate

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from .forms import CommentForm
 
 class ProductListView(generic.ListView):
     model = Product
-    paginate_by =1
+    paginate_by =3
     template_name= 'products/product_list.html'
     context_object_name= 'products'
 
