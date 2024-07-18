21c8be8ba36920bafbf15addb054fd160e370f34
hossein <hossein.h.a770227@gmail.com>
2024-07-18 21:12:58 +0330

    add comment form

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 from .models import Product, Comment
+from .forms import CommentForm
 
 class ProductListView(generic.ListView):
     model = Product
@@ -11,10 +12,17 @@ class ProductListView(generic.ListView):
 
 def product_detail_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    comment = Comment.objects.filter(active=True)
-    return render(request,  'products/product_detail.html', context={'product':product, 'comments':comment})
+    comment = Comment.objects.filter(active=True).order_by('date_time_created')
+    if request.method =='POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.product = product
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form =CommentForm()
+    return render(request,  'products/product_detail.html', context={'product':product, 'comments':comment, 'comment_form':comment_form})
 
-# class ProductDetailView(generic.DetailView):
-#     model = Product
-#     template_name = 'products/product_detail.html'
-#     context_object_name ='product'
+
