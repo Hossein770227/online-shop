8c76eafcf432f4a2527ff6d5878574a2d9105dc2
hossein <hossein.h.a770227@gmail.com>
2024-07-20 00:41:44 +0330

    add verbos name to apps

--- products/admin.py
+++ products/admin.py
@@ -3,13 +3,19 @@ from django.contrib import admin
 from .models import Product, Comment
 
 
+class CommentTabuInline(admin.TabularInline):
+    model = Comment
+    list_display = ['author','active','date_time_created',]
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title','price','active','date_time_created',]
     search_fields = ['title']
+    inlines = [CommentTabuInline]
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['author','product','active','date_time_created',]
-    search_fields = ['author']
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ['author','product','active','date_time_created',]
+#     search_fields = ['author']
