a709e5dd2dc7d7f52002eb15bca242bd78e2608d
hossein <hossein.h.a770227@gmail.com>
2024-07-20 14:25:28 +0330

    add forms.py for app cart

--- cart/views.py
+++ cart/views.py
@@ -5,3 +5,7 @@ from .cart import Cart
 def cart_detail_view(request):
     cart = Cart(request)
     return render(request, 'cart/cart_detail.html',{'cart':cart})
+
+
+def add_to_cart_view(request, product_id):
+    pass
