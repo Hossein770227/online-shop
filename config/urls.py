44e3b50868c8815b079c0044a6b5948d5810da48
hossein <hossein.h.a770227@gmail.com>
2024-07-21 22:52:35 +0330

    add orderform

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')),
     path('call/', include('pages.urls')),
     path('cart/', include('cart.urls')),
+    path('order/', include('orders.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
