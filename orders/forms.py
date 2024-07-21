f441448efc9cfba156977ad3bd94bd410cdf74be
hossein <hossein.h.a770227@gmail.com>
2024-07-21 23:19:43 +0330

    add some translate

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 from .models import Order 
 
@@ -6,3 +7,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['full_name','email','address','order_notes',]
+        widgets = {
+            'address':forms.Textarea(attrs={'rows':4, 'placeholder':_('please enter your address here')}),
+            'order_notes':forms.Textarea(attrs={'rows':3,'placeholder':_('please enter your notes here otherwise Leave blank')}),
+            }
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from .forms import OrderForm
+
 def order_create_view(request):
-    return render(request, 'orders/order_create.html')
+    form = OrderForm()
+    return render(request, 'orders/order_create.html',context={'form':form})
